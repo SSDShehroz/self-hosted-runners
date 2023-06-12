846dd58c07ada67da8975ffe5b022c2aa41a6820
SSDShehroz <shehroz@halimsol.com>
2023-06-12 16:35:49 +0500

    commit test 102

--- secondProject/src/app/app.component.ts
+++ secondProject/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'secondProject test 101';
+  title = 'secondProject test 102';
 }
--- selfHostedRunners/src/app/app.component.ts
+++ selfHostedRunners/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstProject Test 101';
+  title = 'firstProject Test 102';
 }
