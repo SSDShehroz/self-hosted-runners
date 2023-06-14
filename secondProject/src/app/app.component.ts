380d520074daa48bcaaa7f140d910ab22c5b8d53
SSDShehroz <shehroz@halimsol.com>
2023-06-14 16:06:34 +0500

    asdasd

--- secondProject/src/app/app.component.ts
+++ secondProject/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'secondProzject2wzsdsd';
+  title = 'secosssndProzject2wzsdsd';
 }
