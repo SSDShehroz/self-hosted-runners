38ff6f410e7f7de2d45ccbbd01a8c842e9daa9c3
SSDShehroz <shehroz@halimsol.com>
2023-06-15 12:52:15 +0500

    final test

--- selfHostedRunners/src/app/app.component.ts
+++ selfHostedRunners/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstProject Test 104';
+  title = 'Self Hosted Project';
 }
